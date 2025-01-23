d10e1e48b59b660bdaf98abda80bef1da3ac00e9
Srbolseiro802 <pedro.bolseiro802@gmail.com>
2025-01-23 04:28:45 +0000

    trunfo 1

--- cartas trunfo - novato.c
+++ cartas trunfo - novato.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 int main(){
 
-int populacao;
-int turistico;
-float areakm;
+printf("desafio cartas trunfo\n");
+
+float populacao;
+int pontos_turisticos;
+float area;
 float pib;
-char codigocarta;
-char nome;
+char nome[100];
 
-printf
+printf("digite o nome da cidade: \n");
+scanf("%s" , &nome);
 
+printf("digite os pontos turisticos: \n");
+scanf("%d" , &pontos_turisticos);
+
+printf("digite a populacao: \n");
+scanf("%f" , &populacao);
+
+printf("digite o pib: \n");
+scanf("%f" , &pib);
+
+printf("digite sua area: \n");
+scanf("%f" , &area);
+
+printf("Nome da cidade: %s\n" , nome);
+printf("Pontos turisticos: %d\n" , pontos_turisticos);
+printf("População: %f\n" , populacao);
+printf("Pib:%f\n" , pib);
+printf("Área: %f\n" , area);
 
 
 
